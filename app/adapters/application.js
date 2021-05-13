33863969e382367fe772c085a2aad32fb3756486
Alexandre Didion <ale.didion@gmail.com>
2021-05-13 11:11:55 +0200

    use of ember-cli-dotenv

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,10 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import ENV from "../config/environment";
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = 'http://dijkstralex.herokuapp.com/'
   namespace = 'api/v1';
   headers = {
-    'Authorization': ENV[BASIC_SECRET]
+    'Authorization': ENV.BASIC_SECRET
   };
 }
