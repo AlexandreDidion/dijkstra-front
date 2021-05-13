d5347a64eed0589d5fe9cfd77e53b45939a354c0
Alexandre Didion <ale.didion@gmail.com>
2021-05-13 10:27:06 +0200

    Implement basic country front end
    Custom Adapter for application
    Model country.js
    Countries endpoint route (route and template)

---
+++ app/adapters/application.js
@@ -0,0 +1,9 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  host = 'http://dijkstralex.herokuapp.com/'
+  namespace = 'api/v1';
+  headers = {
+    'Authorization': 'Basic YWxleDpkaWprc3RyYWJyYWNhZGFicmE='
+  };
+}
