5ee22de8283e205bc9a9d17898306f60c07b7c7d
Alexandre Didion <ale.didion@gmail.com>
2021-05-24 10:36:56 +0200

    change urlGenerator
    Still to do : improve the drop-down form action, prone to error

--- app/components/drop-down.js
+++ app/components/drop-down.js
@@ -1,9 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 export default class DropDownComponent extends Component {
+  @service router;
 
-  @tracked selectedValue;
+  @tracked selectedValue = 0;
+
+  get urlPath() {
+    return this.router.urlFor(this.args.pathTo,this.selectedValue);
+  }
 
   @action
   selectValue(event) {
