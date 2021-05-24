a8b8f2289811f0b132dc063cf19c0bbf7d084f0b
Alexandre Didion <ale.didion@gmail.com>
2021-05-24 18:07:52 +0200

    Still trying to make form

--- app/components/drop-down.js
+++ app/components/drop-down.js
@@ -1,19 +1,10 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 import { inject as service } from '@ember/service';
 export default class DropDownComponent extends Component {
-  @service router;
+  @service router
 
-  @tracked selectedValue = 0;
-
-  get urlPath() {
-    return this.router.urlFor(this.args.pathTo,this.selectedValue);
-  }
-
-  @action
-  selectValue(event) {
-    let value_id = event.target.value
-    this.selectedValue = value_id
-  }
+  selectId = `select-${guidFor(this)}`;
 }
--- app/routes/countries.js
+++ app/routes/countries.js
@@ -1,10 +1,23 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class CountriesRoute extends Route {
   @service store;
 
+  @tracked selectedValue = 0;
+
   model() {
     return this.store.findAll('country');
   }
+
+  @action
+  selectValue(event) {
+    let value_id = event.target.value;
+    this.selectedValue = value_id;
+    console.log(this.selectedValue);
+  }
+
+  
 }
