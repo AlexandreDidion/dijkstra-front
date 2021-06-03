f4f058bbcca88926a08a53248bd8e52a1c157453
Alexandre Didion <ale.didion@gmail.com>
2021-06-03 16:40:25 +0200

    use power select for countries

--- app/controllers/countries.js
+++ app/controllers/countries.js
@@ -1,4 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class CountriesController extends Controller {
+  @service router;
+
+  @tracked country = this.countries.firstObject;
+
+  get urlCountry() {
+    return this.router.urlFor('country', this.country.id);
+  }
 }
--- app/routes/countries.js
+++ app/routes/countries.js
@@ -1,23 +1,15 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-
 export default class CountriesRoute extends Route {
   @service store;
-
-  @tracked selectedValue = 0;
 
   model() {
     return this.store.findAll('country');
   }
 
-  @action
-  selectValue(event) {
-    let value_id = event.target.value;
-    this.selectedValue = value_id;
-    console.log(this.selectedValue);
-  }
+  setupController(controller, model) {
+    super.setupController(controller, model);
 
-  
+    controller.countries = model;
+  }
 }
