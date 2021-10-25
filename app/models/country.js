bce58ca7f353039b1ff95863099b692fad4766cb
AlexandreDidion <ale.didion@gmail.com>
2021-10-25 11:06:26 +0200

    change front to receive new journey object from end

--- app/models/country.js
+++ app/models/country.js
@@ -2,6 +2,8 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class CountryModel extends Model {
   @attr('string') name;
+  @attr() latitude;
+  @attr() longitude;
 
   @hasMany('city') cities;
   @hasMany('road') roads;
