54b1d11aa7eb56f497275002a194ded7fd68e5be
AlexandreDidion <ale.didion@gmail.com>
2021-10-25 17:45:17 +0200

    make a componenet out of map img

--- app/controllers/country/dijkstra.js
+++ app/controllers/country/dijkstra.js
@@ -1,55 +1,6 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import ENV from '../../config/environment';
 
 export default class CountryController extends Controller {
   @tracked dijkstra = this.dijkstra;
-
-  get mapUrl() {
-    return `https://api.mapbox.com/styles/v1/mapbox/light-v10/static/geojson(${encodeURIComponent(
-      this.geoJson
-    )})/auto/320x320?access_token=${ENV.MAPBOX_API_KEY}`;
-  }
-
-  get coordinates() {
-    let coordinates = new Array();
-    let cities = this.dijkstra.journey;
-    cities.forEach((city) => {
-      coordinates.push([
-        Number(city.longitude.toFixed(3)),
-        Number(city.latitude.toFixed(3)),
-      ]);
-    });
-    return coordinates;
-  }
-
-  get geoJson() {
-    let data = {
-      'type': 'FeatureCollection',
-      'features': [
-        {
-          type: 'Feature',
-          'properties': {
-            'marker-size': 'medium',
-            'marker-color': '#9a1750'
-          },
-          'geometry': {
-            'type': 'MultiPoint',
-            'coordinates': this.coordinates
-          }
-        },
-        {
-          'type': 'Feature',
-          'properties': {
-            'stroke': '#555555'
-          },
-          'geometry': {
-            'type': 'LineString',
-            'coordinates': this.coordinates
-          }
-        }
-      ]
-    }
-    return JSON.stringify(data);
-  }
 }
