492ba2f10e04a799637e13b351cacbb3e6797fee
AlexandreDidion <ale.didion@gmail.com>
2021-10-26 15:08:27 +0200

    add landing page

--- app/router.js
+++ app/router.js
@@ -7,7 +7,8 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('countries', { path: '/' });
+  this.route('home', { path: '/' });
+  this.route('countries');
   this.route('country', { path: '/countries/:country_id' }, function () {
     this.route('dijkstra', { path: '/dijkstra/:dijkstra_id' });
   });
